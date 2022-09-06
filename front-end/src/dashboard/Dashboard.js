42af20e1d540bfaf9cd2d235891e5bf799210d8b
Robbie Woolf <robbiemwoolf@gmail.com>
2022-09-06 14:38:03 -0700

    frontend us01, us02, us03, us07, us08 tests passing

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { listReservations } from "../utils/api";
-import ErrorAlert from "../layout/ErrorAlert";
+import React, { useEffect, useState } from 'react' 
+import ReservationTable from './reservationTable/ReservationTable' 
+import { listReservations } from '../utils/api' 
+import ErrorAlert from '../layout/ErrorAlert' 
+import { useHistory } from 'react-router-dom' 
+import { previous, next } from '../utils/date-time'
+import TableList from './TableList/TableList' 
 
 /**
  * Defines the dashboard page.
@@ -9,30 +13,64 @@ import ErrorAlert from "../layout/ErrorAlert";
  * @returns {JSX.Element}
  */
 function Dashboard({ date }) {
-  const [reservations, setReservations] = useState([]);
-  const [reservationsError, setReservationsError] = useState(null);
+  const [reservations, setReservations] = useState([]) 
+  const [tables, setTables] = useState([])
+  const [reservationsError, setReservationsError] = useState(null) 
+  const history = useHistory()
 
-  useEffect(loadDashboard, [date]);
+  useEffect(loadDashboard, [date]) 
 
   function loadDashboard() {
-    const abortController = new AbortController();
-    setReservationsError(null);
+    const abortController = new AbortController() 
+    setReservationsError(null) 
     listReservations({ date }, abortController.signal)
       .then(setReservations)
-      .catch(setReservationsError);
-    return () => abortController.abort();
+      .then(setTables)
+      .catch(setReservationsError) 
+    return () => abortController.abort() 
+  }
+
+  function handleToday() {
+    history.push(`/dashboard`) 
+  }
+
+  function handlePrev() {
+    const newDate = previous(date) 
+    history.push(`/dashboard?date=${newDate}`) 
+  }
+
+  function handleNext() {
+    history.push(`/dashboard?date=${next(date)}`) 
   }
 
   return (
     <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+      <h1 className='d-md-flex justify-content-center'>Dashboard</h1>
+      <div className='d-md-flex mb-3 justify-content-center'>
+        <h4 className='mb-0'>Reservations for {date}</h4>
+      </div>
+      <div className='pb-2 d-flex justify-content-center'>
+        <button className='btn btn-primary mr-1' onClick={handleToday}>
+          today
+        </button>
+        <button className='btn btn-primary mr-1' onClick={handlePrev}>
+          previous
+        </button>
+        <button className='btn btn-primary' onClick={handleNext}>
+          next
+        </button>
       </div>
       <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <ReservationTable
+        reservations={reservations}
+        setReservations={setReservations}
+        setError={setReservationsError}
+      />
+      <div>
+        <TableList tables={tables} loadDashboard={loadDashboard} />
+      </div>
     </main>
-  );
+  ) 
 }
 
-export default Dashboard;
+export default Dashboard 
--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from 'react' 
 
-import { Redirect, Route, Switch } from "react-router-dom";
-import Dashboard from "../dashboard/Dashboard";
-import NotFound from "./NotFound";
-import { today } from "../utils/date-time";
+import { Redirect, Route, Switch } from 'react-router-dom' 
+import Dashboard from '../dashboard/Dashboard' 
+import Reservations from '../reservations/Reservations' 
+import NotFound from './NotFound' 
+import { today } from '../utils/date-time' 
+import useQuery from '../utils/useQuery' 
+import Tables from '../tables/Tables' 
+import Seat from '../seat/Seat' 
+import Search from '../search/Search' 
+import Edit from '../reservations/Edit' 
 
 /**
  * Defines all the routes for the application.
@@ -12,23 +18,40 @@ import { today } from "../utils/date-time";
  *
  * @returns {JSX.Element}
  */
-function Routes() {
+ function Routes() {
+  const query = useQuery() 
+  const date = query.get('date') 
   return (
     <Switch>
-      <Route exact={true} path="/">
-        <Redirect to={"/dashboard"} />
+      <Route exact={true} path='/'>
+        <Redirect to={'/dashboard'} />
       </Route>
-      <Route exact={true} path="/reservations">
-        <Redirect to={"/dashboard"} />
+      <Route exact path='/reservations/:reservation_id/seat'>
+        <Seat />
       </Route>
-      <Route path="/dashboard">
-        <Dashboard date={today()} />
+      <Route exact path='/reservations/:reservation_id/edit'>
+        <Edit />
+      </Route>
+      <Route exact path='/reservations/new'>
+        <Reservations />
+      </Route>
+      <Route exact={true} path='/reservations'>
+        <Redirect to={'/dashboard'} />
+      </Route>
+      <Route path='/dashboard'>
+        <Dashboard date={date || today()} />
+      </Route>
+      <Route path='/tables/new'>
+        <Tables />
+      </Route>
+      <Route path='/search'>
+        <Search />
       </Route>
       <Route>
         <NotFound />
       </Route>
     </Switch>
-  );
+  ) 
 }
 
-export default Routes;
+export default Routes 

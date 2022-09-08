bb431032980654c8902210815b0105a6e5efb5c2
Robbie Woolf <robbiemwoolf@gmail.com>
2022-09-08 06:21:44 -0700

    url update

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,8 +5,12 @@
  import formatReservationDate from "./format-reservation-date";
  import formatReservationTime from "./format-reservation-date";
  
- const API_BASE_URL =
-   process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+//  const API_BASE_URL =
+//    process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+
+const API_BASE_URL =
+  "https://reservation-backend-rw.herokuapp.com" || "http://localhost:5001";
+
  
  /**
   * Defines the default headers for these functions to work with `json-server`
